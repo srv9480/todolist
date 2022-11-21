f2c0a698aa856fbb141d047d40fa034f4bd2db62
srv9480 <102672439+srv9480@users.noreply.github.com>
2022-11-21 17:33:37 +0300

    ADD добавлены кнопки отдельным компонентом

---
+++ src/Components/Buttons/Button.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const buttonTypes = {
+  primary: "primary",
+  secondary: "secondary",
+};
+
+export default function Button({ children, type, variant, ...rest }) {
+  return (
+    <button
+      className={getClasses([
+        styles.button,
+        styles[`button--${buttonTypes[variant]}`],
+      ])}
+      type={type === "submit" ? "submit" : "button"}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
+
+function SelectButton({ children, ...rest }) {
+  return (
+    <select
+      className={getClasses([styles.button, styles.button__select])}
+      {...rest}
+    >
+      {children}
+    </select>
+  );
+}
+export { SelectButton };
